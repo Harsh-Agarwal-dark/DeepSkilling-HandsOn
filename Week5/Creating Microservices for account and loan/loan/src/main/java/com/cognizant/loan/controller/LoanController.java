cff6c11104698e307008d65462550a8f4a97e9a4
DhoglaCoder <aharsh5498@gmail.com>
2025-07-20 14:40:59 +0530

    Fifth Week Mandatory Hands On Completed

---
+++ Week5/Creating Microservices for account and loan/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,21 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public ResponseEntity<Map<String, Object>> getLoan(@PathVariable String number) {
+        Map<String, Object> loan = new HashMap<>();
+        loan.put("number", number);
+        loan.put("type", "car");
+        loan.put("loan", 400000);
+        loan.put("emi", 3258);
+        loan.put("tenure", 18);
+        return ResponseEntity.ok(loan);
+    }
+}
